aeacf22ecca4a54bd225462658d8bc4baa32d321
reaPeR1010 <reaPeR10x10x@gmail.com>
2021-11-06 08:17:18 +0000

    services.c: Mark 'policydb' as __maybe_unused
    Signed-off-by: reaPeR1010 <reaPeR10x10x@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -735,7 +735,7 @@ static inline int security_validtrans_handle_fail(struct selinux_state *state,
 					   struct context *tcontext,
 					   u16 tclass)
 {
-	struct policydb *p = &state->ss->policydb;
+	struct policydb * __maybe_unused p = &state->ss->policydb;
 #ifdef CONFIG_AUDIT
 	char *o = NULL, *n = NULL, *t = NULL;
 	u32 olen, nlen, tlen;
@@ -1625,7 +1625,7 @@ static inline int compute_sid_handle_invalid_context(
 	u16 tclass,
 	struct context *newcontext)
 {
-	struct policydb *policydb = &state->ss->policydb;
+	struct policydb * __maybe_unused policydb = &state->ss->policydb;
 #ifdef CONFIG_AUDIT
 	char *s = NULL, *t = NULL, *n = NULL;
 	u32 slen, tlen, nlen;
@@ -1941,7 +1941,7 @@ static inline int convert_context_handle_invalid_context(
 	struct selinux_state *state,
 	struct context *context)
 {
-	struct policydb *policydb = &state->ss->policydb;
+	struct policydb * __maybe_unused policydb = &state->ss->policydb;
 #ifdef CONFIG_AUDIT
 	char *s;
 	u32 len;
