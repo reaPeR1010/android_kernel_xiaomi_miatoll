a93402b251c9ce2a1614713f311ddda7cb4ee9b3
reaPeR1010 <reaPeR10x10x@gmail.com>
2022-01-28 08:29:49 +0530

    kernel: fork: Boost CPU LLCC Bus when zygote forks
    Signed-off-by: reaPeR1010 <reaPeR10x10x@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2235,8 +2235,10 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current))
+	if (task_is_zygote(current)) {
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
+                devfreq_boost_kick_max(DEVFREQ_CPU_CPU_LLCC_BW, 50);
+           }
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
